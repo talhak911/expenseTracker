7cc103df750bdb797561887b7f16b84a188b7ec5
talhak911 <talhakhan8300@gmail.com>
2024-03-02 18:27:36 +0500

    height changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import ExpenseTracker from "./components/ExpenseList";
 
 export default function Home(){
   return(
-    
+    <div className="w-screen h-screen">
     <ExpenseTracker/>
+    </div>
   )
 }
